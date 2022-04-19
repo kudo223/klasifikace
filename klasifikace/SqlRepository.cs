eed3842c49c12241cd9adcd3d9295cfcd65d902f
Dominik Kunc <Kunc.Dominik@stud.vassboskovice.cz>
2022-04-19 13:47:47 +0200

    sfasfdasfasfda

--- klasifikace/SqlRepository.cs
+++ klasifikace/SqlRepository.cs
@@ -10,6 +10,11 @@ namespace klasifikace
     public class SqlRepository
     {
         private string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Klasifikace;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private Dictionary<int,Teacher> teachers;
+        public SqlRepository()
+        {
+            teachers = GetTeachersFromDB();
+        }
         private List<Student> TempStudents()
         {
             List<Student> students = new List<Student>();
@@ -65,10 +70,75 @@ namespace klasifikace
             }
             catch (Exception ex)
             {
-                throw new Exception($"Some error happened (Exception: ur mom)" + ex.Message);
+                throw new Exception($"Some error happened (Exception: ur mom and {ex.Message})");
             }
             //return TempStudents();
             return students;
+        }
+        
+        public List<Grade> GetGrades()
+        {
+            List<Grade> grades = new List<Grade>();
+            using(SqlConnection sqlConnection = new SqlConnection(connString))
+            {
+                using (SqlCommand sqlCommand= new SqlCommand("", sqlConnection))
+                {
+                    sqlCommand.CommandText();
+                }
+            }
+            return grades;
+        }
+        private Dictionary<int,Teacher> GetTeachersFromDB()
+        {
+            Dictionary<int, Teacher> teachers = new Dictionary<int,Teacher>();
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(connString))
+                {
+                    sqlConnection.Open();
+                    using (SqlCommand sqlCommand = new SqlCommand())
+                    {
+                        sqlCommand.Connection = sqlConnection;
+                        sqlCommand.CommandText = "SELECT * FROM Teacher";
+                        using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                        {
+                            while (sqlDataReader.Read())
+                            {
+                                var id = Convert.ToInt32(sqlDataReader["IdTeacher"]);
+                                teachers.Add(id,new Teacher()
+                                {
+                                    Id = id,
+                                    Name = sqlDataReader["Name"].ToString(),
+                                    ShortName = sqlDataReader["ShortName"].ToString(),
+                                });
+
+                            }
+                        }
+                    }
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Some error happened (Exception: ur mom and {ex.Message})");
+            }
+            //return TempStudents();
+            return teachers;
+        }
+        public List<Teacher> GetTeachers()
+        {
+            return teachers.SelectMany(t=>t.Value);
+        }
+        public Teacher GetTeacher(int id)
+        {
+            foreach(var teacher in teachers)
+            {
+                if (teacher.Id == id)
+                {
+                    return teacher;
+                }
+            }
+            return null;
         }
     }
 }
